6c06e8b0976a4f7d4989136bd7e201409ac6a49a
Alexey Andreyev <aa13q@ya.ru>
2019-08-23 17:15:44 +0300

    E2EE: implement megolm inbound session decrypt for room

--- lib/room.h
+++ lib/room.h
@@ -26,6 +26,7 @@
 
 #include "events/accountdataevents.h"
 #include "events/encryptedevent.h"
+#include "events/roomkeyevent.h"
 #include "events/roommessageevent.h"
 
 #include <QtCore/QJsonObject>
@@ -191,13 +192,8 @@ public:
     memberCount() const;
     int timelineSize() const;
     bool usesEncryption() const;
-    RoomEventPtr decryptMessage(EncryptedEvent* encryptedEvent);
-    QString decryptMessage(QJsonObject personalCipherObject,
-                           QByteArray senderKey);
-    QString sessionKey(const QString& senderKey, const QString& deviceId,
-                       const QString& sessionId) const;
-    QString decryptMessage(QByteArray cipher, const QString& senderKey,
-                           const QString& deviceId, const QString& sessionId);
+    RoomEventPtr decryptMessage(const EncryptedEvent& encryptedEvent);
+    void handleRoomKeyEvent(RoomKeyEvent* roomKeyEvent, QString senderKey);
     int joinedCount() const;
     int invitedCount() const;
     int totalMemberCount() const;
